e1647c204a293162f8972901f44e03403c729dfa
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2020-10-18 15:03:59 +0200

    Added BE mock and created the project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {   BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import styled from 'styled-components'
+import Shop from './Shop';
+// import Checkout from './Checkout';
+
+
+const AppWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+
+.page-title {
+  font-size: 60px;
+  font-weight: bold;
+  font-family: 'Nixie One', cursive;
+  padding: 20px;
+}
+`
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+       <div className="page-title">Plants Store</div>
+       <Router>
+       <Switch>
+         <Route exact path="/" component={Shop}/>
+         {/* <Route exact path="/checkout" component={Checkout} /> */}
+       </Switch>
+       </Router>
+    </AppWrapper>
   );
 }
 
---
+++ src/api.js
@@ -0,0 +1,25 @@
+export const data = {
+    type: "shoppingcart",
+    shoppingcartid: "a4cde56a-2e71-42ab-a458-b52dc81b0fdd",
+    products: [
+      {
+        productid: "21291d4d-6199-45eb-8549-e941813752f8",
+        title: "Monstera Deliciosa",
+        price: "10.99",
+        amount: 1,
+        image: "/assets/monsteradeliciosa.jpg"
+      },
+      {
+        productid: "f05f4a23-10a1-4f39-a959-bfcbfaa613a3",
+        title: "Photos Ivy",
+        price: "200.99",
+        amount: 1,
+        image: "/assets/photosivy.jpg"
+      }
+    ],
+    sum: "211.98",
+    vadPercent: "19%",
+    vadSum: "49,19",
+    deliveryCosts: "4.99",
+    totalSum: "266.16"
+}
